1b3b8730dc8533958b6cfdac6718c031b08b3024
tushar <tusharshitre123@gmail.com>
2022-02-13 23:43:24 +0530

    add handle null and empty exceptation

---
+++ MoodAnalizer/MoodAnalizer/MoodAnalyzerException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MoodAnalyzer
+{
+    [Serializable]
+    internal class MoodAnalyzerException : Exception
+    {
+        private object eMPTY_EXCEPTION;
+        private string v;
+
+        public MoodAnalyzerException()
+        {
+        }
+
+        public MoodAnalyzerException(string message) : base(message)
+        {
+        }
+
+        public MoodAnalyzerException(object eMPTY_EXCEPTION, string v)
+        {
+            this.eMPTY_EXCEPTION = eMPTY_EXCEPTION;
+            this.v = v;
+        }
+
+        public MoodAnalyzerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MoodAnalyzerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public static object ExceptionType { get; internal set; }
+    }
+}
---
+++ MoodAnalizerTest/MoodAnalyzerException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MoodAnalizerTest
+{
+    [Serializable]
+    internal class MoodAnalyzerException : Exception
+    {
+        public MoodAnalyzerException()
+        {
+        }
+
+        public MoodAnalyzerException(string message) : base(message)
+        {
+        }
+
+        public MoodAnalyzerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MoodAnalyzerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
