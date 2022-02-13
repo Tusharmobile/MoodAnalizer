06f4ff88f917febecb1208a38fb5ab6cf783b08f
tushar <tusharshitre123@gmail.com>
2022-02-13 22:20:28 +0530

    add sub branch refactor ability to analyzer

--- MoodAnalizerTest/AnalyzeMood.cs
+++ MoodAnalizerTest/AnalyzeMood.cs
@@ -4,11 +4,16 @@ namespace MoodAnalyzerTest
 {
     internal class AnalyzeMood
     {
-        public AnalyzeMood()
+        public AnalyzeMood(string v)
         {
         }
 
         internal object Mood(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Mood()
         {
             throw new NotImplementedException();
         }
