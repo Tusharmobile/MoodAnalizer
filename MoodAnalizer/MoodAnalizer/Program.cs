d6df4448eb3cc0f0bba51d7dc1ebcc3c6f973983
tushar <tusharshitre123@gmail.com>
2022-02-13 18:55:07 +0530

    add mood analizer program

---
+++ MoodAnalizer/MoodAnalizer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the mood analizer");
+            Console.ReadLine();
+        }
+    }
+}
