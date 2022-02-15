9d6117126ad6c60cf26152f7f96f352dd61c1a9b
tushar <tusharshitre123@gmail.com>
2022-02-15 18:26:32 +0530

    add reflection with default constractor

--- MoodAnalizer/MoodAnalizer/MoodAnalyzerException.cs
+++ MoodAnalizer/MoodAnalizer/MoodAnalyzerException.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Runtime.Serialization;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace MoodAnalyzer
 {
     [Serializable]
     public class MoodAnalyzerException : Exception
     {
+
         public ExceptionType type;
         //Enumeration
         public enum ExceptionType
         {
             NULL_EXCEPTION,
-            EMPTY_EXCEPTION
+            EMPTY_EXCEPTION,
+            NO_SUCH_CLASS,
+            NO_SUCH_CONSTRUCTOR
         }
         //Exception Constructor.
         public MoodAnalyzerException(ExceptionType type, string message) : base(message)
